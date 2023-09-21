809d4c698f4601acb00644838441abbf36ef9d81
11best <asamaporn.b@appman.co.th>
2023-09-22 01:31:44 +0700

    feat: display cat img

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,24 @@
+import useCatQuery from "../../queries/useCatQuery";
+
 const Home = () => {
+  const catQuery = useCatQuery();
+
+  if (!catQuery) return null;
+
+  if (catQuery.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (catQuery.error) {
+    return <div>error!</div>;
+  }
+
+  console.log("log", catQuery.data);
+
   return (
     <div>
       <div>Home ja</div>
+      <img src={catQuery.data} />
     </div>
   );
 };
--- src/queries/useCatQuery.ts
+++ src/queries/useCatQuery.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 export default function useCatQuery() {
   return useQuery<any>(["cat"], async () => {
     const resp = await fetch("https://cataas.com/cat");
-    // const json = await resp.json();
-    return resp;
+    const blob = await resp.blob();
+    return URL.createObjectURL(blob);
   });
 }
