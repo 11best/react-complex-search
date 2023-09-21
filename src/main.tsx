b4eaba2718d6c21515526a80cc808141f70b8fb6
11best <asamaporn.b@appman.co.th>
2023-09-21 12:29:44 +0700

    feat: example query

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
   </React.StrictMode>
 );
--- src/queries/useCatQuery.ts
+++ src/queries/useCatQuery.ts
@@ -1,15 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 
-export default function useCatQuery(id: string) {
-  return useQuery<any>(
-    ["cat", id],
-    async () => {
-      const resp = await fetch(`/xxx`);
-      const json = await resp.json();
-      return json.props;
-    },
-    {
-      enabled: !!id,
-    }
-  );
+export default function useCatQuery() {
+  return useQuery<any>(["cat"], async () => {
+    const resp = await fetch("https://cataas.com/cat");
+    // const json = await resp.json();
+    return resp;
+  });
 }
