1afa0949a18c5430b1b3d5a0a62f42cd17f22137
11best <asamaporn.b@appman.co.th>
2023-09-22 12:05:07 +0700

    fixed size cat image

--- src/queries/useCatQuery.ts
+++ src/queries/useCatQuery.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 export default function useCatQuery() {
   return useQuery<any>(["cat"], async () => {
-    const resp = await fetch("https://cataas.com/cat");
+    const resp = await fetch("https://cataas.com/cat?width=300&height=300");
     const blob = await resp.blob();
     return URL.createObjectURL(blob);
   });
