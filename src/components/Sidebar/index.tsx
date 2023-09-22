6ca30345cb24e760ff77dcd9f9342049d4cadde2
11best <asamaporn.b@appman.co.th>
2023-09-22 12:55:47 +0700

    add papuan pic as a logo and change title to Papuan

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,20 @@
 import clsx from "clsx";
+import logo from "../../assets/logo/papuan_logo.png";
 
 const Sidebar = () => {
   return (
     <div className="w-60 px-4">
-      <div
-        className={clsx(
-          "text-orange-300 font-bold text-lg h-16 flex items-center",
-          "border-b border-slate-200"
-        )}
-      >
-        Title
+      <div className="flex border-b border-slate-200">
+        <img src={logo} alt="papuan_logo" className="w-8 h-fit mx-2 my-4" />
+        <div
+          className={clsx(
+            "text-orange-300 font-bold text-lg h-16 flex items-center"
+          )}
+        >
+          Papuan
+        </div>
       </div>
+
       <div>sidebar</div>
       <div>sidebar</div>
       <div>sidebar</div>
