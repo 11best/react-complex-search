4157b4e441431c512600f1622d72cc94d7416001
11best <asamaporn.b@appman.co.th>
2023-09-22 15:12:36 +0700

    create mock button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,18 +7,17 @@ const Header = () => {
   return (
     <header className="h-16 flex items-center justify-end">
       <div className="flex gap-4">
+        <button
+          title="nothing here just mockup"
+          className="px-2 py-1 border border-slate-300 rounded-md text-slate-700"
+        >
+          butt
+        </button>
         <div
           title="nothing here just mockup"
           className={clsx(
             "w-8 h-8 bg-slate-300 hover:bg-orange-400 rounded-full",
-            "cursor-pointer duration-300"
-          )}
-        ></div>
-        <div
-          title="nothing here just mockup"
-          className={clsx(
-            "w-8 h-8 bg-slate-300 hover:bg-orange-400 rounded-full",
-            "cursor-pointer duration-300"
+            "cursor-pointer"
           )}
         ></div>
       </div>
