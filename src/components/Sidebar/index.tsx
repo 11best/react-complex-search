2335457facb5be9274f88d767f0a9d6b06e95bdc
11best <asamaporn.b@appman.co.th>
2023-09-22 13:27:59 +0700

    change title and logo to be <a>

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,15 +4,13 @@ import logo from "../../assets/logo/papuan_logo.png";
 const Sidebar = () => {
   return (
     <div className="w-60 px-4">
-      <div className="flex border-b border-slate-200">
-        <img src={logo} alt="papuan_logo" className="w-8 h-fit mx-2 my-4" />
-        <div
-          className={clsx(
-            "text-orange-300 font-bold text-lg h-16 flex items-center"
-          )}
-        >
-          Papuan
-        </div>
+      <div className="border-b border-slate-200">
+        <a href="/" className="flex w-fit">
+          <img src={logo} alt="papuan_logo" className="w-8 h-fit mx-2 my-4" />
+          <p className="text-orange-300 font-bold text-lg h-16 flex items-center">
+            Papuan
+          </p>
+        </a>
       </div>
 
       <div>sidebar</div>
