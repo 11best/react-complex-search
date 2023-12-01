be238430c3957a8eae52a46d6ec1cbeb503fd9f9
11best <asamaporn.b@appman.co.th>
2023-12-01 17:38:37 +0700

    fix svg attributes name

--- src/assets/icons/Chevron.tsx
+++ src/assets/icons/Chevron.tsx
@@ -8,9 +8,9 @@ const Chevron = (props: { className: string }) => {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         d="m9 6 6 6-6 6"
       />
     </svg>
